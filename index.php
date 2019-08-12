449a5e38e8521ee982e0237d0554a28ae213a0ec
Elder Carlos <elderexecute@gmail.com>
2019-08-12 15:13:13 -0300

    alteracao na tabela

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
+
+
 <?php
 
 class index {
 
-public $saldoDeGol;
-public $vitorias;
-public $derrotas;
+public $saldoDeGol = 0;
+
 
 public function tabela(){
-	if($saldoDeGol 0 and $vitorias <= $derrotas){
-		echo "Seu time foi rebaixado";
+	if($saldoDeGol ==0 ){
+		echo " Saldo de gol do seu time é:$saldoDeGol ";
+	} else{
+		echo"Seu timne não tem saldo de gol";
 	}
 
 }
 
 	
 }
+ 
